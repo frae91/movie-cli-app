704d0c7997be1559574b1712581d296fe63a037b
Frae <efvaldehuesa@gmail.com>
2024-05-29 18:59:40 +0800

    Add solution

--- lib/models.py
+++ lib/models.py
@@ -5,22 +5,93 @@ from sqlalchemy.ext.associationproxy import association_proxy
 Base = declarative_base()
 
 class Movie(Base):
-    pass
+    __tablename__ = "movies"
+
+    id = Column(Integer(), primary_key=True)
+    title = Column(String())
+    plot = Column(String())
+
+    movie_genres = relationship('MovieGenre', back_populates='movie')
+    genres = association_proxy('movie_genres', 'genre', creator=lambda g: MovieGenre(genre=g))
+
+    casts = relationship('Cast', back_populates='movie')
+    actors = association_proxy('casts', 'actor', creator=lambda a: Cast(actor=a))
+
+    faves = relationship('Fave', back_populates='movie')
+    users = association_proxy('faves', 'user', creator=lambda u: Fave(user=u))
+
+    def __repr__(self):
+        return f"{self.id}: {self.title}"
 
 class Genre(Base):
-    pass
+    __tablename__ = "genres"
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+
+    movie_genres = relationship('MovieGenre', back_populates='genre')
+    movies = association_proxy('movie_genres', 'movie', creator=lambda m: MovieGenre(movie=m))
+
+    def __repr__(self):
+        return f"{self.name}"
 
 class Actor(Base):
-    pass
+    __tablename__ = "actors"
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+
+    casts = relationship('Cast', back_populates='actor')
+    movies = association_proxy('casts', 'movie', creator=lambda m: Cast(movie=m))
+
+    def __repr__(self):
+        return f"{self.id}: {self.name}"
 
 class MovieGenre(Base):
-    pass
+    __tablename__ = "movie_genres"
+
+    id = Column(Integer(), primary_key=True)
+    movie_id = Column(Integer(), ForeignKey('movies.id'))
+    genre_id = Column(Integer(), ForeignKey('genres.id'))
+
+    movie = relationship('Movie', back_populates='movie_genres')
+    genre = relationship('Genre', back_populates='movie_genres')
 
 class Cast(Base):
-    pass
+    __tablename__ = "casts"
+
+    id = Column(Integer(), primary_key=True)
+    movie_id = Column(Integer(), ForeignKey('movies.id'))
+    actor_id = Column(Integer(), ForeignKey('actors.id'))
+    role = Column(String())
+
+    movie = relationship('Movie', back_populates='casts')
+    actor = relationship('Actor', back_populates='casts')
+
+    def __repr__(self):
+        return f"{self.actor.name} as {self.role}"
 
 class User(Base):
-    pass
+    __tablename__ = "users"
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+
+    faves = relationship('Fave', back_populates='user')
+    movies = association_proxy('faves', 'movie', creator=lambda m: Fave(movie=m))
+
+    def __repr__(self):
+        return f"{self.id}: {self.name}"
 
 class Fave(Base):
-    pass
+    __tablename__ = 'faves'
+
+    id = Column(Integer(), primary_key=True)
+    movie_id = Column(Integer(), ForeignKey('movies.id'))
+    user_id = Column(Integer(), ForeignKey('users.id'))
+
+    movie = relationship('Movie', back_populates='faves')
+    user = relationship('User', back_populates='faves')
+
+    def __repr__(self):
+        return f"{self.movie.id}. {self.movie.title}"
--- lib/run.py
+++ lib/run.py
@@ -37,8 +37,128 @@ def main_menu():
     print("\nPlease enter you menu choice:")
     return input()
 
+def login(username):
+    clear()
+    user = session.query(User).filter(User.name == username).first()
+
+    if not user:
+        user = User(name=username)
+        session.add(user)
+        session.commit()
+        print("You have successfully registered to this app!")
+    else:
+        print(f"Welcome back, {user.name}!")
+
+    global logged_user
+    logged_user = user
+
 def greet():
-    pass
+    clear()
+
+    print("Welcome to the Movie CLI App!")
+
+    print("\nPlease enter your username:")
+    username = input()
+
+    login(username)
+
+def add_to_fave(movie):
+    choice = handle_yes_no("\nDo you want to add this to your watchlist (yes/no)?")
+
+    if choice:
+        new_fave = Fave(movie_id=movie.id, user_id=logged_user.id)
+        session.add(new_fave)
+        session.commit()
+        input("\nYou have successfully added this to your watchlist! Press enter to continue...")
+
+
+def display_movies(movies, heading_msg):
+    loop = True
+    while loop:
+        heading(heading_msg)
+
+        if len(movies)>0:
+            for movie in movies:
+                print(movie)
+        else:
+            print("No movie...")
+
+        print('-'*30)
+        print("\nPlease enter the movie ID to view more details (type 'back' to go back):")
+        id_input = input()
+
+        if id_input.lower() != 'back':
+            movie_id = int(id_input)
+
+            movie = [ m for m in movies if m.id == movie_id ]
+
+            if len(movie)>0:
+                movie = movie[0]
+                clear()
+
+                print(f"{movie.title.upper()}")
+                print(f"Genres:")
+                print(movie.genres)
+                print(f"Cast:")
+                for actor in movie.actors:
+                    cast = session.query(Cast).filter(Cast.actor_id==actor.id, Cast.movie_id==movie.id).first()
+                    print(cast)
+
+                print("Plot:")
+                print(f"{movie.plot}")
+
+                add_to_fave(movie)
+            else:
+                print("No movie found!")
+        
+        else:
+            clear()
+            loop = False
+            return False
+
+def all():
+    clear()
+    movies = session.query(Movie).all()
+
+    return display_movies(movies, "ALL MOVIES")
+
+def faves():
+    clear()
+
+    heading("WATCH LIST")
+
+    if logged_user.movies:
+        for fave_movie in logged_user.movies:
+            print(fave_movie)
+    else:
+        print("No movie in the watchlist!")
+
+    input("\nPress enter to continue...")
+    clear()
+
+def genre():
+    print("\nPlease enter a genre:")
+    genre_input = input()
+
+    genre = session.query(Genre).filter(Genre.name.like(genre_input)).first()
+
+    if genre:
+        clear()
+        display_movies(genre.movies, f"MOVIES UNDER {genre.name.upper()}")
+    else:
+        print("No genre found!")
+
+def actor():
+    print("\nPlease enter the actor's name:")
+    actor_input = input()
+
+    actor = session.query(Actor).filter(Actor.name.like(actor_input)).first()
+
+    if actor:
+        clear()
+        display_movies(actor.movies, f"MOVIES OF {actor.name.upper()}")
+    else:
+        print("No actor found!")
 
 def start():
     greet()
@@ -47,12 +167,20 @@ def start():
         choice = main_menu()
         while True:
             if choice.lower() == 'all':
+                clear()
+                all()
                 break
             elif choice.lower() == 'genre':
+                clear()
+                genre()
                 break
             elif choice.lower() == 'actor':
+                clear()
+                actor()
                 break
             elif choice.lower() == 'faves':
+                clear()
+                faves()
                 break
             elif choice.lower() == 'exit' or choice.lower() == 'quit':
                 loop = False
